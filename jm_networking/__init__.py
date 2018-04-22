89df3738db47010fca09cd61dff3070d00b122a0
Jack Miller <jack@Jacks-MacBook-Pro.local>
2018-04-22 11:54:42 -0500

    clean up for publishing

--- jm_networking/__init__.py
+++ jm_networking/__init__.py
@@ -17,22 +17,22 @@ class Network:
         self.on_failure_callback = callback
 
     def get(self, url):
-        self.log(f"Attempting GET {url}")
+        self.log("Attempting GET " + url)
         req = requests.get(url)
         return self.finish(req)
 
     def put(self, url, data):
-        self.log(f"Attempting PUT {url}")
+        self.log("Attempting PUT " + url)
         req = requests.put(url, data)
         return self.finish(req)
 
     def post(self, url, data):
-        self.log(f"Attempting POST {url}")
+        self.log("Attempting POST " + url)
         req = requests.post(url, data)
         return self.finish(req)
 
     def delete(self, url):
-        self.log(f"Attempting DELETE {url}")
+        self.log("Attempting DELETE " + url)
         req = requests.delete(url)
         return self.finish(req)
 
@@ -40,7 +40,7 @@ class Network:
         if result.status_code < 400:
             return self.on_success_callback(result)
         else:
-            self.log(f"ERROR ({result.status_code}): {result.text}", error=True)
+            self.log(result.status_code + ": " + result.text, error=True)
             return self.on_failure_callback(result)
 
     def __enter__(self):
